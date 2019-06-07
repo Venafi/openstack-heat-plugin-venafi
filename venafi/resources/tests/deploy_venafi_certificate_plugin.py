5b9a36aa45b770f1d5006b758dcc40fdea60ef5a
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2019-06-07 10:30:35 +0300

    change deploy

--- venafi/resources/tests/deploy_venafi_certificate_plugin.py
+++ venafi/resources/tests/deploy_venafi_certificate_plugin.py
@@ -3,15 +3,15 @@ import time
 from invoke import UnexpectedExit
 import os
 
-c = fabricConnection(os.environ['OS_HOST'])
+c = fabricConnection(os.environ['DEVSTACK_HOST'])
 msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
 pwd = os.path.dirname(os.path.abspath(__file__))
 os.system('rsync --delete --exclude ".git" --exclude ".venv" --exclude ".idea" --exclude "__pycache__" --exclude '
           '"*.pyc" '
           '-pthrvz  '
           ''+pwd+
-          '/../../../../venafi-openstack-heat-plugin '+os.environ['OS_HOST_USER']+'@'+os.environ[
-              'OS_HOST']+':/usr/lib/heat')
+          '/../../../../venafi-openstack-heat-plugin '+os.environ['DEVSTACK_USER']+'@'+os.environ[
+              'DEVSTACK_HOST']+':/usr/lib/heat')
 result = c.run('sudo systemctl restart devstack@h-eng')
 print(msg.format(result))
 # TODO: rewrite sleep to check of "systemctl status devstack@h-eng"
