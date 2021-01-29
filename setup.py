3220f22ddf3b7fff8c1b3e658b6488638ff176e1
Ryan Treat <tr1ck3r@users.noreply.github.com>
2021-01-28 17:10:15 -0800

    Update long_description to use README

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2019 Venafi, Inc.
+# Copyright 2019-2021 Venafi, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,19 +15,31 @@
 # limitations under the License.
 #
 
+import pathlib
 from __future__ import absolute_import, print_function, unicode_literals
 from setuptools import setup
 from setuptools import find_packages
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name='openstack-heat-plugin-venafi',
-    description='Venafi Inc. OpenStack Heat Plugin',
-    long_description='Venafi Inc. OpenStack Heat Plugin',
+    description='OpenStack Heat Plugin for Venafi Trust Protection Platform and Venafi Cloud',
+    long_description=README,
+    long_description_content_type="text/markdown",
     license='Apache License, Version 2.0',
     version=u"0.1.1",
     author='Venafi Inc.',
     author_email='opensource@venafi.com',
-    url='https://github.com/Venafi/openstack-heat-plugin-venafi',
+    url='https://www.venafi.com/',
+    project_urls={
+        'Source Code': 'https://github.com/Venafi/openstack-heat-plugin-venafi',
+        'Bug Tracker': 'https://github.com/Venafi/openstack-heat-plugin-venafi/issues',
+    },
     keywords=['venafi', 'openstack', 'heat', 'ssl', 'certificates'],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
